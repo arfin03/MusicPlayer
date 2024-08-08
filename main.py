393cd2bc1c4a4dbb18cc93b4e755ef6ea58db4e6
ishu9805 <abhishek47903@gmail.com>
2024-08-08 20:06:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ from core import (
 REPO = """
 🤖 **Music Player**
 
-- Repo: [GitHub](https://github.com/AsmSafone/MusicPlayer)
+- Repo: [GitHub](https://github.com/ishu9805/MusicPlayer)
 - License: AGPL-3.0-or-later
 """
 
