38cd4034d4037c9bec89ba9f26c00b9fd3f509dc
Maou Arfin <156934678+arfin03@users.noreply.github.com>
2024-08-11 16:55:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ from core import (
 REPO = """
 🤖 **Music Player**
 
-- Repo: [GitHub](https://github.com/ishu9805/MusicPlayer)
+- Repo: [GitHub](https://github.com/AsmSafone/MusicPlayer)
 - License: AGPL-3.0-or-later
 """
 
